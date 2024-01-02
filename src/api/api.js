a8c17befa3c9ac496b36effc90ce76a13266b1cf
yavan <yavankumar2@gmail.com>
2024-01-02 20:57:48 +0530

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+const apiKey = "cb62ec6ed988011a7aa21606e6993196";
+
+const getWeather = async (city) => {
+  return await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      return json;
+    });
+};
+
+export default getWeather;
